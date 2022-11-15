b2b2e23671d5eaeb40a91c71290a34f9ffe69867
兰戈 <2380584338@qq.com>
2022-11-15 20:18:42 +0800

    update

---
+++ src/main/java/com/rango/ReverseList.java
@@ -0,0 +1,33 @@
+package com.rango;
+
+import com.rango.obj.ListNode;
+
+import java.util.Arrays;
+
+/**
+ * @see <a href="https://leetcode.cn/problems/reverse-linked-list/">链接</a>
+ */
+public class ReverseList {
+
+    public static void main(String[] args) {
+//        ListNode n1 = new ListNode(1);
+//        ListNode n2 = new ListNode(2);
+//        ListNode n3 = new ListNode(3);
+//        ListNode n4 = new ListNode(4);
+//        ListNode n5 = new ListNode(5);
+//        n4.setNext(n5);
+//        n3.setNext(n4);
+//        n2.setNext(n3);
+//        n1.setNext(n2);
+//        ListNode data = reverseList_1(n1);
+//        System.out.println(data);
+    }
+
+    public static ListNode reverseList_1(ListNode head) {
+        ListNode pre = null;
+        ListNode cur = head;
+        while (cur != null) {
+        }
+        return pre;
+    }
+}
--- src/main/java/com/rango/obj/ListNode.java
+++ src/main/java/com/rango/obj/ListNode.java
@@ -8,10 +8,15 @@ public class ListNode {
 
     ListNode next;
 
-    public ListNode() {
+    ListNode() {
     }
 
-    public ListNode(int val) {
+    ListNode(int val) {
         this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
     }
 }
