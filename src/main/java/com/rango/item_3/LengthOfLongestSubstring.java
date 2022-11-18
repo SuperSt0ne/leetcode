fa52367ddeb8c7fb928a9db590eeb3ecab1145f2
兰戈 <2380584338@qq.com>
2022-11-18 18:02:23 +0800

    add: 无重复字符的最长子串

---
+++ src/main/java/com/rango/item_3/LengthOfLongestSubstring.java
@@ -0,0 +1,32 @@
+package com.rango.item_3;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 无重复字符的最长子串
+ *
+ * @see <a href="https://leetcode.cn/problems/longest-substring-without-repeating-characters/">...</a>
+ */
+public class LengthOfLongestSubstring {
+    public static void main(String[] args) {
+        System.out.println(lengthOfLongestSubstring("erwlerwex"));
+    }
+
+    public static int lengthOfLongestSubstring(String s) {
+        if (s.length() == 0) {
+            return 0;
+        }
+        Map<Character, Integer> map = new HashMap<>();
+        int max = 0, left = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (map.containsKey(s.charAt(i))) {
+                left = Math.max(left, map.get(s.charAt(i)) + 1);
+            }
+            map.put(s.charAt(i), i);
+            max = Math.max(max, i - left + 1);
+        }
+        return max;
+    }
+
+}
