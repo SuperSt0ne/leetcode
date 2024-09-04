4cd26f0a40d9f9ca88b19dcdd2ce8ddba26e8a3f
兰戈 <2380584338@qq.com>
2024-09-04 16:59:40 +0800

    add: num 151 ans

--- src/main/java/com/rango/leetcode/N0151ReverseWords.java
+++ src/main/java/com/rango/leetcode/N0151ReverseWords.java
@@ -6,10 +6,29 @@ package com.rango.leetcode;
 public class N0151ReverseWords {
 
     public static void main(String[] args) {
-
+        String a = " hello world  ";
+        System.out.println(reverseWords(a));
+        String b = "the sky is blue";
+        System.out.println(reverseWords(b));
+        String c = "a good   example";
+        System.out.println(reverseWords(c));
     }
 
     public static String reverseWords(String s) {
-        return "";
+        StringBuilder ans = new StringBuilder();
+        for (int i = s.length() - 1; i >= 0; i--) {
+            while (i >= 0 && s.charAt(i) == ' ') {
+                --i;
+            }
+            int end = i;
+            if (i < 0) {
+                break;
+            }
+            while (i >= 0 && s.charAt(i) != ' ') {
+                --i;
+            }
+            ans.append(" ").append(s, i + 1, end + 1);
+        }
+        return ans.substring(1);
     }
 }
