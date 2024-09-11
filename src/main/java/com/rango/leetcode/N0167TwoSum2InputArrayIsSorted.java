ef192f4db26db0ecbd6e56ff1a4f19b2f881d406
兰戈 <2380584338@qq.com>
2024-09-11 15:18:20 +0800

    add: num 167 question and answer

---
+++ src/main/java/com/rango/leetcode/N0167TwoSum2InputArrayIsSorted.java
@@ -0,0 +1,31 @@
+package com.rango.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * <a href="https://leetcode.cn/problems/two-sum-ii-input-array-is-sorted/">167. 两数之和 II - 输入有序数组</a>
+ */
+public class N0167TwoSum2InputArrayIsSorted {
+
+    public static void main(String[] args) {
+        int[] numbers = {2, 7, 11, 15};
+        int target = 9;
+        System.out.println(Arrays.toString(twoSum(numbers, target)));
+    }
+
+    public static int[] twoSum(int[] numbers, int target) {
+        int left = 0, right = numbers.length - 1;
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+            if (sum == target) {
+                return new int[] {left + 1, right + 1};
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return new int[] {-1, -1};
+    }
+
+}
