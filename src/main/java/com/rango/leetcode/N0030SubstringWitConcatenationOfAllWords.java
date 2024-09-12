fcb6a3adb7c80bca622a3af801c63eb2af7d849a
兰戈 <2380584338@qq.com>
2024-09-12 18:14:26 +0800

    add: num 30 question

---
+++ src/main/java/com/rango/leetcode/N0030SubstringWitConcatenationOfAllWords.java
@@ -0,0 +1,18 @@
+package com.rango.leetcode;
+
+import java.util.List;
+
+/**
+ * <a href="https://leetcode.cn/problems/substring-with-concatenation-of-all-words">30.串联所有单词的子串</a>
+ */
+public class N0030SubstringWitConcatenationOfAllWords {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static List<Integer> findSubstring(String s, String[] words) {
+        return null;
+    }
+
+}
